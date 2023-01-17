9681dc051afb5335371eb581360c4f442d438e6d
mrnakordome <Aliamini838383@gmail.com>
2023-01-17 22:52:08 +0330

    direx+fileex

--- project.c
+++ project.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+int check_direx(char name[]){
+    DIR* dir = opendir(name);
+    if(dir){
+        closedir(dir);
+        return 1;
+    }
+    else
+        return 0;
+}
+
+int check_fileex(char name[]){
+    FILE* file = fopen(name, "r");
+    if(file){
+        fclose(file);
+        return 1;
+    }
+    else
+        return 0;
+}
 
 int main(){
 
