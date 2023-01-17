e52bd409b39d474cf7f054c8b6b8663006ec6376
mrnakordome <Aliamini838383@gmail.com>
2023-01-18 00:28:05 +0330

    createfile

--- project.c
+++ project.c
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int check_direx(char name[]){
     DIR* dir = opendir(name);
@@ -26,8 +28,67 @@ int check_fileex(char name[]){
         return 0;
 }
 
-int main(){
+void createfile(char name[]){
+    int n = strlen(name) , counter=0;
+    if(name[0] == '"'){
+        printf("%s %d\n",name,n);
+        n-=2;
+        for(int i=0;i<n;i++)
+            name[i]=name[i+1];
+        name[n] = '\0';
+        printf("%s %d\n",name,n);
+    }
+    char direction[100] ;
+    for(int i=0;i<n;i++){
+        if(name[i] != '/'){
+            direction[i]=name[i];
+        }
+        else{
+            direction[i] = '\0';
+            if(check_direx(direction)){
+                direction[i] = '/';
+            }
+            else{
+                mkdir(direction);
+                direction[i] = '/';
+            }
+        }
+    }
+    if(check_fileex(name))
+        printf("File already exists\n");
+    else{
+        FILE* file;
+        file = fopen(name , "w");
+        fclose(file);
+    }
+}
 
+
+int main(){
+    char vo[100] , c;
+    while(1){
+        scanf("%s",vo);
+        if(!strcmp(vo , "createfile")){
+            getchar();
+            scanf("%s",vo);
+            if(strcmp(vo,"--file")){
+                fgets(vo,100,stdin);
+                printf("Invalid Input\n");
+            }
+            else{
+                fgets(vo,100,stdin);
+                vo[strlen(vo)-1] = '\0';
+                for(int i=0;i<strlen(vo)-1;i++)
+                    vo[i]=vo[i+1];
+                vo[strlen(vo)-1] = '\0';
+                createfile(vo);
+            }
+        }
+        else{
+            printf("Invalid Input\n");
+            fgets(vo,100,stdin);
+        }
+    }
     return 0;
 }
 
